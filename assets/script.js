fa116937c323a749b63684bc608ab757ba765508
Victor Santos <victorufrb@hotmail.com>
2022-08-26 11:42:28 -0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){
+
+    $(".mudaTela").click(function(){
+        mudaTela( $(this), $(this).attr("nova"), $(this).attr("animacao"), $(this).attr("tempoAnimacao") );
+    });
+
+    $("a.opcoes").click(function(e){
+        e.preventDefault();
+        $("div.opcoes").slideToggle(500);
+    });
+
+    $(".calendario .marcado").click(function(){
+        mostraMsgMes($(this).attr("value"));
+    });
+
+    const mudaTela = ( atual, nova = null, animacao = "fade", tempoAnimacao = 900 ) => {
+
+        // define a nova tela
+        if(!nova){
+            nova = parseInt(atual.parent().attr("id").split("tela")[1])+1;
+        }
+
+        if(animacao == "fade"){
+            $("#tela"+(nova-1)).fadeOut(tempoAnimacao);
+            setTimeout(() => {
+                $("#tela"+nova).fadeIn(tempoAnimacao)
+            }, tempoAnimacao);
+        }else{
+            $("#tela"+(nova-1)).hide(tempoAnimacao);
+            $("#tela"+nova).show(tempoAnimacao);
+        }
+
+        if($("#tela"+nova).hasClass("temporizado")){
+            $("#tela"+nova+" div").hide();
+            telaTemporizada(nova, 0);
+        }
+
+        verificaFundo(nova);
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        if(nova == 2){
+            var audio = new Audio('assets/musica.mp3');
+            audio.volume = 0.1;
+            audio.play();
+        }
+        
+    }
+
+    const telaTemporizada = ( nTela, contador ) =>{
+
+        const tela = $("#tela"+nTela+" div:eq("+contador+")");
+        const temporizador = 500;
+        const temporizadorPrimeiraTela = (contador==0?$("#tela"+nTela).attr("tempo"):temporizador);
+
+        setTimeout(() => {
+            tela.fadeIn(temporizador);
+
+            setTimeout(() => {
+                tela.fadeOut(temporizador);
+                if(tela.attr("final") == "true"){
+                    mudaTela(null, nTela+1, "fade", 900);
+                    verificaFundo(nTela+1);
+                }else{
+                    telaTemporizada(nTela, contador+1);
+                }
+
+            }, tela.attr("tempo") );
+
+        }, temporizadorPrimeiraTela);
+        
+    }
+
+    const verificaFundo = (nTela) =>{
+
+        const fundo = $("#tela"+nTela).attr("fundo");
+        const tempo = $("#tela"+nTela).attr("tempo");
+
+        if(fundo){
+            $("body").attr("class", fundo);            
+        }
+        
+    }
+
+    const mostraMsgMes = (texto) =>{
+
+        let titulo;
+        let mensagem;
+
+        switch(texto){
+            case "9/6": titulo = "09 de Junho de 2022"; mensagem = "<p>Esse foi o dia que nos falamos pela primeira vez, eu indicando minha série preferida (mas já cheio de segundas intenções hehehe). </p><p>Admirava suas fotos todo santo dia, que mulher DAORA 💙</p>";break;
+            case "11/6": titulo = "11 de Junho de 2022"; mensagem = "<p>O dia do primeiro migué que joguei pra puxar assunto, falando que te conhecia de algum lugar. Agora posso falar que era tudo mentira, só queria falar mais com você :3 </p>";break;
+            case "14/6": titulo = "14 de Junho de 2022"; mensagem = "<p>Descobri que na real a senhorita se chamava ANNA CLARA, meu nome preferido. Apesar de você não gostar, saiba que eu gosto mto mto mto dele kkkkkk</p>";break;
+            case "16/6": titulo = "16 de Junho de 2022"; mensagem = "<p>O dia que eu \"me acheguei demais\" kkkkkkkk. Tentei ser mais ousado no flerte e acabei tendo que disfarçar pra não passar vergonha.</p>";break;
+            case "29/5": titulo = "29 de Junho de 2022"; mensagem = "<p></p>";break;
+            case "3/6": titulo = "03 de Junho de 2022"; mensagem = "<p></p>";break;
+            case "12/6": titulo = "12 de Junho de 2022"; mensagem = "<p></p>";break;
+            case "13/6": titulo = "13 de Junho de 2022"; mensagem = "<p></p>";break;
+            case "19/6": titulo = "19 de Junho de 2022"; mensagem = "<section class='text-center'><p class='letra-vermelha'><strong>Este momento está sendo escrito agora...</strong></p></section>";break;
+            case "final": titulo = "19 de Junho de 2022"; mensagem = "<section class='text-center mt-5 mb-5'><p><strong>O dia em que ela disse<br><span class='letra2 letra-vermelha'>SIM</span></strong></p></section>";break;
+        }
+
+        mostraPopUp(true, titulo, mensagem);
+        telaFinal = (texto=="final"?true:false);
+    }
+
+    
+
+});
+
+let telaFinal = false;
+
+const mostraPopUp = (mostrar, titulo = "Título de testes", mensagem = "Mensagem de teste...") =>{
+
+    if(mostrar){
+        $("html, body").animate({ scrollTop: $(".pop-up")[0].offsetTop }, "smooth");
+        $(".pop-up").fadeIn(500);
+        $(".pop-up h1").html(titulo);
+        $(".pop-up div").html(mensagem);
+        $(".container").css("opacity", "0.5");
+    }else{
+        $(".pop-up").fadeOut(500);
+        $(".container").css("opacity", "1");
+
+        if(telaFinal){
+            $("#tela19").fadeOut(4000);
+            setTimeout(() => {
+                $("#tela20").fadeIn(6500);
+                $("body").attr("class", "fundo6");    
+                $("html, body").animate({ scrollTop: 0 }, "slow");
+            }, 4000);
+        }
+
+    }
+
+}
